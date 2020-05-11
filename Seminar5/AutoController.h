0d9d2ed6b19eb8380bba93bc59477825b350e4d2
rosianucarla <61684170+rosianucarla@users.noreply.github.com>
2020-05-11 23:01:59 +0300

    Add project files.

---
+++ Seminar5/AutoController.h
@@ -0,0 +1,22 @@
+#pragma once
+#include "CrudRepo.h"
+#include "Wagen.h"
+#include "Kunde.h"
+
+class AutoController
+{
+public:
+	AutoInMemoryRepository repo;
+	//~AutoController();
+	Wagen* findAutoById(int id);
+	vector<Wagen*> findAll();
+	Wagen* saveAuto(Wagen* a);
+	Wagen* updateAuto(Wagen* a);
+	Wagen* deleteAuto(int id);
+	void sort_autos();
+	static AutoController* ret_instanta();
+private:
+	AutoController();
+	static AutoController* instanta;
+};
+
---
+++ Seminar5/ui.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "AutoController.h"
+
+class ui
+{
+private:
+	AutoController* cont;
+public:
+	ui();
+	~ui();
+	void menu();
+	void run();
+};
+
